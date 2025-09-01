0caa463a6ab45dbbf926d64d2a9dd2773fb52005
santhiya777 <santhiyapponnusamy@gmail.com>
2025-09-01 11:16:50 +0530

    Create IStatement.java

---
+++ eclipse-workspace/main2.java/src/IStatement.java
@@ -0,0 +1,10 @@
+
+	//Program to demonstrate Functional Interface
+
+	package com.tnsif.daynineteen;
+
+	@FunctionalInterface // interface is declared with only one abstract method
+	interface IStatement {
+		public String show();// abstract Method
+		
+	}
